fa3f4c82ca54b0d379840c29f67d5f6ef073aa1f
fabiopereiraz <fabio.pereira@icarros.com.br>
2024-08-04 07:25:22 -0300

    inicial

---
+++ lib/tela.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/status.dart' as status;
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class Tela1 extends StatefulWidget {
+  const Tela1({super.key});
+  
+
+  @override
+  State<Tela1> createState() => _Tela1State();
+}
+
+class _Tela1State extends State<Tela1> {
+
+
+  
+  @override
+  void initState()  {
+    super.initState();
+      final wsUrl2 = Uri.parse('wss://stream.binance.com:9443/ws/btcusdt@trade');
+      final wsUrlx = Uri.parse('ws://localhost:8080/websocket');
+      final wsUrl = Uri.parse('ws://localhost:8080/gs-guide-websocket');
+    final channel = WebSocketChannel.connect(wsUrl);
+
+  
+
+    streamListener(channel);
+    
+  }
+
+  streamListener(channel) {
+
+    channel.stream.listen((message) {
+      Map getData = jsonDecode(message);
+      setState(() {
+        
+        btcUsdtPrice = getData['p'];
+      });
+      print(getData['p']);
+      // channel.sink.add('subscribe:geral');
+       channel.sink.add(jsonEncode({
+        'subscribe': '/topic/greetings'
+       }));
+
+
+    }, onError: (error) {
+      print('Error: $error');
+    }, onDone: () {
+      print('Done');
+    });
+
+
+  }
+
+  String btcUsdtPrice = "0";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: const Text('Tela 1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "BTC/USDT Preço ",
+              style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 30),
+            ),
+            Text(
+              btcUsdtPrice,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 250, 194, 25),fontSize: 30
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
